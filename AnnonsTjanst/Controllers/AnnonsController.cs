53d4a240ba9db0a0f60874e8d56f50bb852a6026
Gympztah <Gympztah@users.noreply.github.com>
2019-06-02 08:13:26 +0200

    Authorize added. Hard-coded login credentials: admin + pw

--- AnnonsTjanst/Controllers/AnnonsController.cs
+++ AnnonsTjanst/Controllers/AnnonsController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace AnnonsTjanst.Controllers
 {
-
+    [Authorize]
     public class AnnonsController : Controller
     {
         public ActionResult Skapa()
--- AnnonsTjanst/Controllers/HomeController.cs
+++ AnnonsTjanst/Controllers/HomeController.cs
@@ -11,71 +11,69 @@ namespace AnnonsTjanst.Controllers
     {
 
 
-
+        [Authorize]
         public ActionResult Index(int? idin)
         {
-            if (idin == null)
-            {
-                try
-                {
-                    int id = 11;
-                    loginReferences.InloggningServiceClient logclient = new loginReferences.InloggningServiceClient();
-                    if (Session["profilId"] != null)
-                    {
-                        //Converting your session variable value to integer
+            /* if (idin == null)
+             {
+                 try
+                 {
+                     int id = 11;
+                     loginReferences.InloggningServiceClient logclient = new loginReferences.InloggningServiceClient();
+                     if (Session["profilId"] != null)
+                     {
+                         //Converting your session variable value to integer
 
-                        id = Convert.ToInt32(Session["profilId"]);//ut komenterad kod pga problem med sekson   Convert.ToInt32(id1.Text); ((int)Session["profilId"]);
-                        var anvendare = logclient.VisaAnvandarInfoId(id);
-                        ViewBag.medalande = anvendare.Anvandarnamn;
-                    }
+                         id = Convert.ToInt32(Session["profilId"]);//ut komenterad kod pga problem med sekson   Convert.ToInt32(id1.Text); ((int)Session["profilId"]);
+                         var anvendare = logclient.VisaAnvandarInfoId(id);
+                         ViewBag.medalande = anvendare.Anvandarnamn;
+                     }
 
-                }
-                catch
-                {
+                 }
+                 catch
+                 {
 
-                }
-            }
-            else
-            {
-                int id = int.Parse(idin.ToString());
-                loginReferences.InloggningServiceClient logclient = new loginReferences.InloggningServiceClient();
-                if (logclient.VerifieraInloggning(id))
-                {
-                    Session["profilId"] = id;
-                    var anvendare = logclient.VisaAnvandarInfoId(id);
-                    string test = anvendare.Anvandarnamn;
+                 }
+             }
+             else
+             {
+                 int id = int.Parse(idin.ToString());
+                 loginReferences.InloggningServiceClient logclient = new loginReferences.InloggningServiceClient();
+                 if (logclient.VerifieraInloggning(id))
+                 {
+                     Session["profilId"] = id;
+                     var anvendare = logclient.VisaAnvandarInfoId(id);
+                     string test = anvendare.Anvandarnamn;
 
-                }
-            }
+                 }
+             }*/
             ServiceReference1.Service1Client client = new ServiceReference1.Service1Client();
             return View(client.HamtaAllaAnnonser());
         }
 
+
         public ActionResult logain()
         {
-            loginReferences.InloggningServiceClient logclient = new loginReferences.InloggningServiceClient();
-            var test = logclient.VerifieraInloggning(1337);
+            
 
             return View();
         }
-        [HttpPost]
-        public ActionResult logain(string anvandarnamn, string losenord, string behorrighet)
-        {
-            if (behorrighet == null)
-            {
-                behorrighet = "standard";
-            }
-            loginReferences.InloggningServiceClient logclient = new loginReferences.InloggningServiceClient();
-            var anvinfo = logclient.LoggaIn(anvandarnamn, losenord, behorrighet);
-            if (anvinfo != null)
-            {
 
-                Session["profilId"] = anvinfo.ProfilId.ToString(); //) ["profilId"] = Convert.ToInt32(anvinfo.ProfilId.ToString());
-                Session["inlogad"] = "true";
+        [HttpPost]
+        public ActionResult logain(string anvandarnamn, string losenord)
+        {
+
+            bool valid = false;
+            valid = System.Web.Security.FormsAuthentication.Authenticate(anvandarnamn, losenord);
+            if (valid)
+            {
+                System.Web.Security.FormsAuthentication.RedirectFromLoginPage(anvandarnamn,false);
             }
+            
             return RedirectToAction("Index");
         }
 
+        [Authorize]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
